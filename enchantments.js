b1a5d40782a4c363e05b8acf27f355fd40b12003
Mochaharpy <166887671+Mochaharpy@users.noreply.github.com>
2024-10-12 13:32:56 -0700

    Create enchantments.js

---
+++ enchantments.js
@@ -0,0 +1,13 @@
+const enchantments = [
+    "protection", "fire_protection", "feather_falling", 
+    "blast_protection", "projectile_protection", "thorns", 
+    "sharpness", "smite", "bane_of_arthropods", 
+    "knockback", "fire_aspect", "looting", 
+    "efficiency", "silk_touch", "unbreaking", 
+    "fortune", "power", "punch", 
+    "flame", "infinity", "luck_of_the_sea", 
+    "lure", "frost_walker", "depth_strider", 
+    "respiration", "aqua_affinity", "mending", 
+    "vanishing_curse", "binding_curse", "soul_speed",
+    "swift_sneak", "wind_burst", "breach", "density"
+];
